674516c1cb8022d6d38e980a5af17179361a4009
somin <qkrthals0524@naver.com>
2020-09-22 17:56:11 +0900

    about State

--- src/App.js
+++ src/App.js
@@ -1,80 +1,27 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-// food 컴포넌트에 kimchi라는 값으로 prop fav을 줌 (property)
-//이렇게 하면 food 함수에 fav=kimchi가 보내짐 console.log로 확인가능 
-const foodILike = [
-  {
-    id :1,
-    name : "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating:5
-  },
-  {
-    id :2,
-    name : "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating:4.9
-  },
-  {
-    id :3,
-    name: "Bibimbap",
-    image :
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating:4.8
-  },
-  {
-    id :4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating:5.5
-  },
-  {
-    id :5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating :4.7
+
+class App extends React.Component {
+  //react class component에서 가져온다(삼성폰을 개발할때 cell phone에서 가져오는것처럼)
+  state = {
+    count: 0,
+  };
+  add = () => {
+    //함수임
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-
-];
-
-function Food({name , picture, rating}) {
-  return (
-  <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src ={picture} alt={name} />
-  </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-         key ={dish.id}
-         name = {dish.name}
-         picture={dish.image}
-         rating ={dish.rating}
-         />
-      ))}
-      
-     
-
-    </div>
-  );
 }
 
 export default App;
- 
