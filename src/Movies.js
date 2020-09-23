fb4f2d5fc62c77bf9c9b3089a1465c822e8773c9
somin <qkrthals0524@naver.com>
2020-09-23 17:52:28 +0900

    Movie App

---
+++ src/Movies.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
+      </div>
+    </div>
+  );
+}
+//movie 컴포넌트로 가서 id, year,, poster까지 준비
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+//우리가 얻어 올 props를 찾는 과정(api를 보고)
+
+export default Movie;
