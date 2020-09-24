fbb0356ceaea76982ef3dde62cba92ec074f1225
somin <qkrthals0524@naver.com>
2020-09-24 15:57:41 +0900

    Routing Bouns Finish

--- src/App.js
+++ src/App.js
@@ -1,55 +1,20 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movies";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false }); //movies:movies 하나는 state에서, 하나는 axios에서 옴
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state; //state로부터 movie를 가져와야함
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((
-              movie //movies array에서 각 item을 뽑아낸게 movie
-            ) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-      //map으로 만들 무언가를 return 해야함
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation></Navigation>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
